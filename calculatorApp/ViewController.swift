4b6736fe21af0597de0e7592ace398447f97a245
islombek <islombek.pulatov9@gmail.com>
2022-12-17 21:27:44 +0500

    Initial Commit

---
+++ calculatorApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  calculatorApp
+//
+//  Created by Ислам Пулатов on 12/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
